ee3f39c316127c8e340cb1750b82218f0d595a15
Felix Han <felix@nowhere.com>
2015-06-18 17:40:56 +0900

    awfea

--- musl/src/string/memcmp.c
+++ musl/src/string/memcmp.c
@@ -1,10 +1,42 @@
 #include <string.h>
+#include <srp_vector.h>
 
+#define __vec_equal(a, b) 
 int musl_memcmp(const void *vl, const void *vr, size_t n)
 {
-	const unsigned char *l=vl, *r=vr;
+	//const unsigned char *l=vl, *r=vr;
+	//for (; n && *l == *r; n--, l++, r++);
+	//return n ? *l-*r : 0;
 
-#pragma rpcc cgra
-	for (; n && *l == *r; n--, l++, r++);
-	return n ? *l-*r : 0;
+  if (n == 0) return 1;
+  long16* lvector = vl, *rvector = vr;
+  long16 zeros = (long16)(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+  size_t nqq;
+  size_t nq = nqq = n / sizeof(long16);
+  size_t nr = n % sizeof(long16);
+
+  if(nq != 0) {
+    nr += sizeof(long16);
+
+    #pragma rpcc cgra
+    for (; nq; nq--, lvector++, rvector++) {
+      long16 ne =  (*lvector != *rvector);
+      long* lptr = ne;
+      int i;
+      for (i = 0; i < 16; i++) {
+        if (lptr[i] != 0) {
+          goto done;
+        }
+      }
+    }
+  }
+done:
+
+  lvector--; rvector--;
+	unsigned char *l=lvector, *r=rvector;
+
+  #pragma rpcc cgra
+	for (; nr && *l == *r; nr--, l++, r++);
+	return nr ? *l-*r : 0;
+
 }
